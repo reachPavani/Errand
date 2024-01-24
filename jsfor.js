7488aaaf106e667ae60b35631c5833759d0aff6c
reachPavani <reachpavani9@gmail.com>
2024-01-24 12:57:03 +0000

    24/01

---
+++ jsfor.js
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>JsForLoop</title>
+    </head>
+    <body>
+        <script>
+            let element="Hello"
+            let index=5;
+            for (let index = 0; index < 5; index++) {
+               /* const element = array[index];*/
+                
+            }
+            console.log('element');
+        </script>
+    </body>
+</html>
